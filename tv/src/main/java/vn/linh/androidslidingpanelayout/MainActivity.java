4ac9c6dfdff95c56b2dbf31e4479d5ce45dfaa22
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-09-28 15:17:15 +0700

    first commit

---
+++ tv/src/main/java/vn/linh/androidslidingpanelayout/MainActivity.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package vn.linh.androidslidingpanelayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import vn.linh.androidslidingpanelayout.fragment.Fragment1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        replace();
+    }
+
+    private void replace(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                Fragment1.newInstance()).commit();
+    }
+
+    View currentFocus;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(currentFocus!=null){
+            currentFocus.setBackgroundColor(Color.TRANSPARENT);
+        }
+        currentFocus = getCurrentFocus();
+        currentFocus.setBackgroundColor(Color.RED);
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ tv/src/main/java/vn/linh/androidslidingpanelayout/fragment/Fragment1.java
@@ -0,0 +1,28 @@
+package vn.linh.androidslidingpanelayout.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import vn.linh.androidslidingpanelayout.R;
+
+public class Fragment1 extends Fragment {
+
+    public static Fragment1 newInstance() {
+         Bundle args = new Bundle();
+         Fragment1 fragment = new Fragment1();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
+    }
+}
